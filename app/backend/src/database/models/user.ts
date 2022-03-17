5617e772491a24e62b802262f7a5c632b56c5ba2
bel <mariaisabelalbuquerque@gmail.com>
2022-03-17 18:04:28 -0300

    cria model user

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class User extends Model {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
