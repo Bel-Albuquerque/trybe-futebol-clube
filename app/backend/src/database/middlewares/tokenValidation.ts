c43558b0c34319ea41120e72df50663ceda8f71c
bel <mariaisabelalbuquerque@gmail.com>
2022-03-17 18:12:17 -0300

    cria pasta de middlewares com arquivo de validaçoes JWT

---
+++ app/backend/src/database/middlewares/tokenValidation.ts
@@ -0,0 +1,24 @@
+import * as jwt from 'jsonwebtoken';
+import fs from 'fs';
+
+export interface MessageErro {
+  error: string,
+}
+
+const JWT_SECRET = fs.readFileSync('jwt.evaluation.key', 'utf8');
+
+interface Data { id: number, username: string, role: string }
+
+export const generateToken = (data: Data) => {
+  const token = jwt.sign(data, JWT_SECRET, { algorithm: 'HS256' });
+  return token;
+};
+
+export const decoder = async (token: string): Promise<MessageErro | string | jwt.JwtPayload> => {
+  try {
+    const decoded: jwt.JwtPayload | string = await jwt.verify(token, JWT_SECRET);
+    return decoded;
+  } catch (err) {
+    return 'token inválido';
+  }
+};
