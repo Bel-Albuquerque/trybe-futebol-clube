49a7b6c3b022562c5add6851a0d3e025649a0a6e
bel <mariaisabelalbuquerque@gmail.com>
2022-03-17 17:55:58 -0300

    cria migration maths

---
+++ app/backend/src/database/migrations/20220316191619-match.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matchs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'clubs',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      home_team_goals: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      away_team: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'clubs',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      away_team_goals: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matchs');
+  }
+};
