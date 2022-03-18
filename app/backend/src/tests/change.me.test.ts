88ee97c1f7ccd8683c50147f13572d68ec859556
bel <mariaisabelalbuquerque@gmail.com>
2022-03-18 13:08:28 -0300

    começa a implementar os testes

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -3,8 +3,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
-
+import User from '../database/models/user';
 import { Response } from 'superagent';
 
 chai.use(chaiHttp);
@@ -16,29 +15,48 @@ describe('Seu teste', () => {
    * Exemplo do uso de stubs com tipos
    */
 
-  // let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
+const mock = [
+  {
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  },
+  {
+    id: 2,
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+  }
+]
+interface IUser {
+  id: number,
+  username: string,
+  role: string,
+  email: string,
+  password: string
+}
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+  before(async () => {
+    sinon
+      .stub(User, "findAll")
+      .resolves(mock as any[]);
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  after(() => {
+    (User.findAll as sinon.SinonStub).restore();
+  })
 
   // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  //   chaiHttpResponse = await chai.request(app)
 
   //   expect(...)
   // });
 
   it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+    expect(false).to.be.eq(false);
   });
 });
