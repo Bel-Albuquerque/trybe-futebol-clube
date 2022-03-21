3bdabb39c59fa68d3bda1e016ebad781539d1dbc
bel <mariaisabelalbuquerque@gmail.com>
2022-03-21 19:18:54 -0300

    corrige erros de lint

--- app/backend/src/database/middlewares/tokenValidation.ts
+++ app/backend/src/database/middlewares/tokenValidation.ts
@@ -9,7 +9,7 @@ const JWT_SECRET = fs.readFileSync('jwt.evaluation.key', 'utf8');
 
 interface Data { id: number, username: string, role: string }
 
-type Decoded = MessageErro | Data | boolean | string;
+// type Decoded = MessageErro | Data | boolean | string;
 
 export const generateToken = (data: Data | string) => {
   const token = jwt.sign(data, JWT_SECRET, { algorithm: 'HS256' });
