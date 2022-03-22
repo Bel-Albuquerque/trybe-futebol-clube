37c5d6cb6eb74458088b4b22789c194b9f8ab4f1
bel <mariaisabelalbuquerque@gmail.com>
2022-03-22 14:36:20 -0300

    corrige erros de validaçao de body

--- app/backend/src/database/middlewares/login-validations.ts
+++ app/backend/src/database/middlewares/login-validations.ts
@@ -7,12 +7,16 @@ const userSchema = Joi.object({
   password: Joi.string().required().min(7),
 });
 
-const errorMessage = () => ({ message: 'All fields must be filled' });
+const errorMessage = (param: string) => {
+  return { message: 'Incorrect email or password' };
+};
 
 export const postLoginValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body
+  if ( !email || !password) return res.status(401).json({ message: 'All fields must be filled' })
   const { error } = userSchema.validate(req.body);
   if (error) {
-    return res.status(401).json(errorMessage());
+    return res.status(401).json(errorMessage(error.details[0].type));
   }
   next();
 };
