d62f1005ebcfa52017b7dfcbdace231c35c7f86e
bel <mariaisabelalbuquerque@gmail.com>
2022-03-27 14:54:27 -0300

    implementa as rotas GET /clubs e GET /clubs/:id

--- app/backend/src/router.ts
+++ app/backend/src/router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getValidateLogin, postLoginController } from './database/controller/login-controller';
 import { postLoginValidation } from './database/middlewares/login-validations';
+import { getAllClubsController, getByIdClubController } from './database/controller/clubs-controller';
 
 const router = express.Router();
 
@@ -11,5 +12,13 @@ router
 router
   .route('/login')
   .post(postLoginValidation, postLoginController);
+
+router
+  .route('/clubs/:id')
+  .get(getByIdClubController);
+
+router
+  .route('/clubs')
+  .get(getAllClubsController);
 
 export default router;
