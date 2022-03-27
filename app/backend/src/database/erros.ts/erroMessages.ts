e9f3da8ec0027aee68cbc75286a77f2f23ab5b08
bel <mariaisabelalbuquerque@gmail.com>
2022-03-27 20:12:40 -0300

    corrige mensagem de erro

--- app/backend/src/database/erros.ts/erroMessages.ts
+++ app/backend/src/database/erros.ts/erroMessages.ts
@@ -9,4 +9,4 @@ export const fildsMustBeFilled = { message: 'All fields must be filled' };
 export const isNotPossibleTwoEqualTeams = (
   { message: 'It is not possible to create a match with two equal teams' });
 
-export const teamNotFound = { message: 'Team not found' };
+export const teamNotFound = { message: 'There is no team with such id!' };
