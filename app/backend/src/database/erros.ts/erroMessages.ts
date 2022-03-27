afe12cbdae8c8536e6fb3b7c4b69f990e66ed171
bel <mariaisabelalbuquerque@gmail.com>
2022-03-27 19:21:42 -0300

    adiciona mensagens de erro ao arquivo erroMessages

--- app/backend/src/database/erros.ts/erroMessages.ts
+++ app/backend/src/database/erros.ts/erroMessages.ts
@@ -2,4 +2,11 @@ export const emailOrPasswodInvalid = { message: 'Incorrect email or password' };
 
 export const joiMessageErro = (err: any) => ({ message: err.message });
 
-export const xablau = 'xablau';
+export const incorrectEmailOrPassword = { message: 'Incorrect email or password' };
+
+export const fildsMustBeFilled = { message: 'All fields must be filled' };
+
+export const isNotPossibleTwoEqualTeams = (
+  { message: 'It is not possible to create a match with two equal teams' });
+
+export const teamNotFound = { message: 'Team not found' };
