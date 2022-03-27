a760286fbc46e9334607568482a8478d15bda14d
bel <mariaisabelalbuquerque@gmail.com>
2022-03-27 17:05:40 -0300

    edita funçao getAllMatchsController para conferir se tem query params

--- app/backend/src/database/controller/matchs-controller.ts
+++ app/backend/src/database/controller/matchs-controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
-import { getAllMatchsService } from '../service/matchs-service';
+import { getAllMatchsService, getTrueOrFalseMatchsService } from '../service/matchs-service';
 
 export const getAllMatchsController = async (req: Request, res: Response) => {
-  const getAllMatchs = await getAllMatchsService();
-  if (getAllMatchs) return res.status(200).json(getAllMatchs);
+  const { inProgress } = req.query;
+  if (inProgress === undefined) {
+    const getAllMatchs = await getAllMatchsService();
+    return res.status(200).json(getAllMatchs);
+  }
+  if (inProgress) {
+    const getMatchs = await getTrueOrFalseMatchsService(inProgress);
+    return res.status(200).json(getMatchs);
+  }
   return res.status(401).end();
 };
-export const xablau = 'xablau';
+
+export const getTrueMatchsController = 'xablau';
