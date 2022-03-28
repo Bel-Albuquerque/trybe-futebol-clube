7495311186946311a331d0a87ec04da1f35304d4
bel <mariaisabelalbuquerque@gmail.com>
2022-03-27 23:21:56 -0300

    adiciona mais testes para a rota match

--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -4,8 +4,10 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { Response } from 'superagent';
 import Matchs from '../database/models/match'
-import { allMatchs, inProgressTrueMatchs, inProgressFalseMatchs } from './mocks/matchsMock';
+import { allMatchs, inProgressTrueMatchs, inProgressFalseMatchs, createMatchs, addMatch, updateMatchs, updatedGoalsMatchs, updateGoals } from './mocks/matchsMock';
 chai.use(chaiHttp);
+import matchService from '../database/service/matchs-service'
+import { userResponse } from './mocks/userMock';
 
 const { expect } = chai;
 ''
@@ -64,7 +66,7 @@ describe('Rota /matchs?inProgress=true', () => {
 });
 
 describe('Rota /matchs?inProgress=true', () => {
-  let matchsNotInProgressResponse: Response
+  let matchsNotInProgressResponse: Response;
   
   before(async () => {
     sinon
@@ -89,3 +91,84 @@ describe('Rota /matchs?inProgress=true', () => {
   });
 });
 
+describe('Rota POST /matchs', () => {
+  let createMatchResponse: Response;
+  
+  before(async () => {
+    sinon
+      .stub(Matchs, 'create')
+      .resolves(createMatchs as any);
+    sinon
+      .stub(matchService, 'checkClubsIds')
+      .resolves(true);  
+    createMatchResponse = await chaiRequestLib(app)
+      .post('/matchs')
+      .set({ Authorization: userResponse.token })
+      .send( addMatch );
+  });
+
+  after(() => {
+    (matchService.checkClubsIds as sinon.SinonStub).restore();
+    (Matchs.create as sinon.SinonStub).restore();
+  })
+
+  it('Testa que é possível criar uma nova partida', () => {
+    const { body } = createMatchResponse;
+    
+    expect(createMatchResponse).to.be.an('object');
+    expect(createMatchResponse).to.have.status(201);
+    expect(body.id).to.be.equal(5);
+  });
+
+});
+
+describe('Rota PATCH /matchs/5/finish', () => {
+  let updateMatchResponse: Response;
+  
+  before(async () => {
+    sinon
+      .stub(Matchs, 'update')
+      .resolves(updateMatchs as any);
+    updateMatchResponse = await chaiRequestLib(app)
+      .patch('/matchs/5/finish');
+  });
+
+  after(() => {
+    (Matchs.update as sinon.SinonStub).restore();
+  })
+
+  it('Testa que é possível editar o "inProgress" pra false de uma partida', () => {
+    const { body } = updateMatchResponse;
+    
+    expect(updateMatchResponse).to.be.an('object');
+    expect(updateMatchResponse).to.have.status(200);
+    expect(body.id).to.be.equal(5);
+  });
+
+});
+
+describe('Rota PATCH /matchs/:id', () => {
+  let updateGoalsMatchResponse: Response;
+  
+  before(async () => {
+    sinon
+      .stub(Matchs, 'update')
+      .resolves(updatedGoalsMatchs as any);
+    updateGoalsMatchResponse = await chaiRequestLib(app)
+      .patch('/matchs/5')
+      .send(updateGoals);
+  });
+
+  after(() => {
+    (Matchs.update as sinon.SinonStub).restore();
+  })
+
+  it('Testa que é possível editar o "inProgress" pra false de uma partida', () => {
+    const { body } = updateGoalsMatchResponse;
+    
+    expect(updateGoalsMatchResponse).to.be.an('object');
+    expect(updateGoalsMatchResponse).to.have.status(200);
+    expect(body.id).to.be.equal(5);
+  });
+
+});
