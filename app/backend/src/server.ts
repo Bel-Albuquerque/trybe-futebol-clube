2a5d4621037fd37e910c02b196a6712196e9514c
bel <mariaisabelalbuquerque@gmail.com>
2022-04-01 14:28:14 -0300

    apaga comentários "inuteis" no arquivo server.ts

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -4,12 +4,3 @@ import 'dotenv/config';
 const PORT = process.env.PORT || 3001;
 const newApp = new App();
 newApp.start(PORT);
-
-// admin: {
-//   validAdmin: {
-//     id: 1,
-//     username: 'Admin',
-//     role: 'admin',
-// admin@admin.com
-//     password: 'secret_admin',
-//   },
